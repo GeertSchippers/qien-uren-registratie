8313ff0a756d8a2137ac4cedd4ab1fe2212d7cf8
Raymon den Hartogh <raydenhartogh@gmail.com>
2018-04-03 16:30:21 +0200

    first commit

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/Uren_registratie', function(){
+    return 'Uren registratie tabel';
+});
---
+++ app/Uren_registratie.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Uren_registratie extends Model
+{
+    //
+}

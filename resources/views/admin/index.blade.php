dd429c114cef6231db4b5e121f9c2460c311663e
Geert Schippers <g.j.f.m.@gmail.com>
2018-04-06 14:22:56 +0200

    users in table zonder admins

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,18 +1,37 @@
 @extends('welcome')
 @section('content')
+<style>
+    th, td{
+        border: 1px solid;
+        padding: 5px;
+    }  
+</style>
     <h1>hello Admin</h1>
-    @if(count($users) > 1)
+    <table>
+        <tr>
+            <th>first name </th>
+            <th>last name </th>
+            <th>email </th>
+            <th>employee number </th>
+            <th>company id </th>
+        </tr>
         @foreach($users as $user)
-            <td class="well">
-                @if($user->admin == 0)
-                
-                @endif
-            </div>
+        
+            @if($user->admin == 0)
+                    <tr>
+                        <td>{{$user->first_name}}</td>
+                        <td>{{$user->last_name}}</td>
+                        <td>{{$user->email}}</td>
+                        <td>{{$user->employee_number}}</td>
+                            @foreach($companies as $company)
+                                @if ($company->id == $user->company_id)
+                                    <td>{{$company->name}}</td>
+                                @endif
+                            @endforeach
+                    </tr>       
+            @endif
         @endforeach
-    @else
-        <p>geen gebruikers gevonden</p>
-    @endif
-    
+    </table>
     <h2> gemaakte uren</h2>
     @if(count($hours) > 1)
         @foreach($hours as $hour)

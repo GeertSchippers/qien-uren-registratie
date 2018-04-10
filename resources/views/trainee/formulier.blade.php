45a8caf217a3ba7994fff2f7a94937673a264d22
Raymon den Hartogh <raydenhartogh@gmail.com>
2018-04-10 15:58:56 +0200

    kleine update

--- resources/views/trainee/formulier.blade.php
+++ resources/views/trainee/formulier.blade.php
@@ -56,7 +56,7 @@
             <div id=extraform></div>
                 
             <td><input type="button" value='voer in' id="submit" onclick=send()></td>
-            <?= Auth::id() ?>
+           
         <div class="tab">
           <button class="tablinks" onclick="openCity(event, 'London')" id="defaultOpen">Review</button>
           <button class="tablinks" onclick="openCity(event, 'Paris')">Goedgekeurd</button>
@@ -81,20 +81,20 @@
     <script>
     function openCity(evt, cityName) {
         var i, tabcontent, tablinks;
-        tabcontent = document.getElementsByClassName("tabcontent");
-        for (i = 0; i < tabcontent.length; i++) {
-            tabcontent[i].style.display = "none";
+            tabcontent = document.getElementsByClassName("tabcontent");
+            for (i = 0; i < tabcontent.length; i++) {
+                tabcontent[i].style.display = "none";
         }
-        tablinks = document.getElementsByClassName("tablinks");
-        for (i = 0; i < tablinks.length; i++) {
-            tablinks[i].className = tablinks[i].className.replace(" active", "");
+            tablinks = document.getElementsByClassName("tablinks");
+            for (i = 0; i < tablinks.length; i++) {
+                tablinks[i].className = tablinks[i].className.replace(" active", "");
         }
-        document.getElementById(cityName).style.display = "block";
-        evt.currentTarget.className += " active";
+            document.getElementById(cityName).style.display = "block";
+            evt.currentTarget.className += " active";
     }
 
-    // Get the element with id="defaultOpen" and click on it
-    document.getElementById("defaultOpen").click();
+            // Get the element with id="defaultOpen" and click on it
+            document.getElementById("defaultOpen").click();
     
     
     
